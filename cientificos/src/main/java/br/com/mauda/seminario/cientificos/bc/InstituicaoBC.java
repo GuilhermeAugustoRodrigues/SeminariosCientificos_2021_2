96090b5066ada8907ff6d5c7458f7ed87314bcd9
guilhermino <guilherme.5337@gmail.com>
2021-09-15 21:42:09 -0300

    Refactor

--- cientificos/src/main/java/br/com/mauda/seminario/cientificos/bc/InstituicaoBC.java
+++ cientificos/src/main/java/br/com/mauda/seminario/cientificos/bc/InstituicaoBC.java
@@ -4,7 +4,9 @@ import br.com.mauda.seminario.cientificos.dao.InstituicaoDAO;
 import br.com.mauda.seminario.cientificos.model.Instituicao;
 
 public class InstituicaoBC extends PatternCrudBC<Instituicao, InstituicaoDAO> {
-    public static InstituicaoBC getInstance() {return instance;}
+    public static InstituicaoBC getInstance() {
+        return instance;
+    }
 
     private static final InstituicaoBC instance = new InstituicaoBC();
 
--- cientificos/src/main/java/br/com/mauda/seminario/cientificos/bc/ProfessorBC.java
+++ cientificos/src/main/java/br/com/mauda/seminario/cientificos/bc/ProfessorBC.java
@@ -4,7 +4,9 @@ import br.com.mauda.seminario.cientificos.dao.ProfessorDAO;
 import br.com.mauda.seminario.cientificos.model.Professor;
 
 public class ProfessorBC extends PatternCrudBC<Professor, ProfessorDAO> {
-    public static ProfessorBC getInstance() {return instance;}
+    public static ProfessorBC getInstance() {
+        return instance;
+    }
 
     private static final ProfessorBC instance = new ProfessorBC();
 
